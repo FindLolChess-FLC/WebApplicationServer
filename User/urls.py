cca47d83cb591a41648331ee311188bfb799d976
gns0314 <gns0314@naver.com>
2024-08-27 02:56:11 +0900

    ✨ : url패턴 소문자로 통일 및 이메일, 닉네임 중복체크 구현

--- User/urls.py
+++ User/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import SignInView, SignUpView, UpdateNicknameView, UpdatePasswordView, LogoutView
+from .views import SignInView, SignUpView, UpdateNicknameView, UpdatePasswordView, SignOutView, EmailDuplicateView, NicknameDuplicateView
 
 urlpatterns = [
-    path('SignUp/', SignUpView.as_view()), # 회원가입
-    path('SignIn/', SignInView.as_view()), # 로그인
-    path('Logout/', LogoutView.as_view()), # 로그아웃
-    path('UpdateInfo/', UpdateNicknameView.as_view()), # 유저 이메일, 닉네임 조회, 비밀번호 수정
-    path('UpdatePassword/', UpdatePasswordView.as_view()), # 비밀번호 변경
+    path('signup/', SignUpView.as_view()), # 회원가입
+    path('signin/', SignInView.as_view()), # 로그인
+    path('signout/', SignOutView.as_view()), # 로그아웃
+    path('updateinfo/', UpdateNicknameView.as_view()), # 유저 이메일, 닉네임 조회, 비밀번호 수정
+    path('updatepassword/', UpdatePasswordView.as_view()), # 비밀번호 변경
+    path('emailduplicate/', EmailDuplicateView.as_view()), # 이메일 중복 체크
+    path('nickduplicate/', NicknameDuplicateView.as_view()), # 닉네임 중복 체크
 ]
