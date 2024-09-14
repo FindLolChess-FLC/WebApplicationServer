35ae370ddca19b86c4cb8a8e1266f66aa97957d1
gns0314 <gns0314@naver.com>
2024-09-15 03:32:42 +0900

    ✨ : 챔피언 조회 기능 구현

---
+++ Meta/views.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Champion
+from .serializer import ChampionSerializer
+
+# 챔피언 조회
+class ChampionSearch(APIView):
+    def get(self, request):
+        name = request.query_params.get('name')
+        if name:
+            serializer = ChampionSerializer(Champion.objects.get(name = request.query_params.get('name')))       
+            return Response({'resultcode': 'SUCCESS', 'data': serializer.data}, status=status.HTTP_200_OK)
+        else:
+            champions = Champion.objects.all()
+            serializer = ChampionSerializer(champions, many=True)
+            return Response({'resultcode': 'SUCCESS', 'data': serializer.data}, status=status.HTTP_200_OK)
+        
