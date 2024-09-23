b7a5397a73e17b167a7d85dd2664e266f2639c10
gns0314 <gns0314@naver.com>
2024-09-24 02:57:42 +0900

    🎨 : FE 요청에 따라 URL 변경

--- User/urls.py
+++ User/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('signup/', SignUpView.as_view()), # 회원가입
     path('signin/', SignInView.as_view()), # 로그인
     path('signout/', SignOutView.as_view()), # 로그아웃
-    path('updateinfo/', UpdateNicknameView.as_view()), # 유저 이메일, 닉네임 조회, 비밀번호 수정
+    path('updateinfo/', UpdateNicknameView.as_view()), # 유저 이메일, 닉네임 조회, 변경
     path('updatepassword/', UpdatePasswordView.as_view()), # 비밀번호 변경
     path('emailduplicate/', EmailDuplicateView.as_view()), # 이메일 중복 체크
-    path('nickduplicate/', NicknameDuplicateView.as_view()), # 닉네임 중복 체크
+    path('nicknameduplicate/', NicknameDuplicateView.as_view()), # 닉네임 중복 체크
     path('deleteid/', DeleteIdView.as_view()), # 회원탈퇴
     path('verification/', EmailVerification.as_view()), # 인증 코드 발급 / 인증
 ]
