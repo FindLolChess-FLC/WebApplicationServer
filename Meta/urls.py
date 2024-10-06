cfb3db340e4d87c5e419274dcead5ec3ed32bf3e
gns0314 <gns0314@naver.com>
2024-10-06 21:23:58 +0900

    ✨ : 증강체 검색 구현

--- Meta/urls.py
+++ Meta/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ChampionSearch, SynergySearch, ItemSearch, MetaSearch
+from .views import ChampionSearch, SynergySearch, ItemSearch, MetaSearch, AugmenterSearch
 
 urlpatterns = [
     path('championsearch/', ChampionSearch.as_view()), # 챔피언 조회
     path('synergysearch/', SynergySearch.as_view()), # 시너지 조회
     path('itemsearch/', ItemSearch.as_view()), # 아이템 조회
     path('metasearch/', MetaSearch.as_view()), # 메타 조회
+    path('augmentersearch/', AugmenterSearch.as_view()), # 증강체 조회
 ]
