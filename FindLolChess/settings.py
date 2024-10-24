b55bbf4466fe1bd25c2416c3cf2d85c023f5bbb9
gns0314 <gns0314@naver.com>
2024-10-24 20:46:37 +0900

    🔥 : 장고 소셜로그인 라이브러리 제거

--- FindLolChess/settings.py
+++ FindLolChess/settings.py
@@ -84,7 +84,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'social_django.middleware.SocialAuthExceptionMiddleware',
 ]
 
 ROOT_URLCONF = 'FindLolChess.urls'
