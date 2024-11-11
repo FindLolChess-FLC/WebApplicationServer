b1207a3c2571f1bdce8664e5d37fe7d85c7bee40
gns0314 <gns0314@naver.com>
2024-11-12 00:25:07 +0900

    ✨ : db 크롤 데이터 제거 구현

---
+++ Crawling/management/commands/clear_db.py
@@ -0,0 +1,20 @@
+from Meta.models import *
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        Synergy.objects.all().delete()
+        SynergyImg.objects.all().delete()
+        Augmenter.objects.all().delete()
+        AugmenterImg.objects.all().delete()
+        Item.objects.all().delete()
+        ItemImg.objects.all().delete()
+        Champion.objects.all().delete()
+        ChampionImg.objects.all().delete()
+        LolMeta.objects.all().delete()
+        MetaReaction.objects.all().delete()
+        LolMetaChampion.objects.all().delete()
+        Comment.objects.all().delete()
+
+
