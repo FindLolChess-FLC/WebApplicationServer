1a8949224c8df96fb3e722965d141683d7908a78
gns0314 <gns0314@naver.com>
2024-11-12 21:46:20 +0900

    🐛: 스웨거 경로 버그 수정

--- FindLolChess/urls.py
+++ FindLolChess/urls.py
@@ -33,8 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('user/', include('User.urls')),
     path('meta/', include('Meta.urls')),
