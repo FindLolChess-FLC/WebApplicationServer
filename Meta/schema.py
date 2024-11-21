c3d16645dbfcc21f7633f87a1b4c68fdcff4d45f
gns0314 <gns0314@naver.com>
2024-11-22 02:02:42 +0900

    🔥 : 패치에 따른 증강체 제거

--- Meta/schema.py
+++ Meta/schema.py
@@ -5,24 +5,6 @@ lol_meta_schema = openapi.Schema(
     properties={
         'id': openapi.Schema(type=openapi.TYPE_INTEGER, description='메타 ID', example=2),
         'title': openapi.Schema(type=openapi.TYPE_STRING, description='메타 제목', example="[상징] 섬뜩한힘 브라이어"),
-        'augmenter': openapi.Schema(
-            type=openapi.TYPE_ARRAY,
-            description='증강체 목록',
-            items=openapi.Schema(
-                type=openapi.TYPE_OBJECT,
-                properties={
-                    'name': openapi.Schema(type=openapi.TYPE_STRING, description='증강체 이름', example="마법지팡이"),
-                    'effect': openapi.Schema(type=openapi.TYPE_STRING, description='증강체 효과', example="쓸데없이 큰 지팡이를 획득합니다. 아군 유닛의 주문력이 18 증가합니다."),
-                    'tier': openapi.Schema(type=openapi.TYPE_STRING, description='증강체 티어', example="Gold"),
-                    'img': openapi.Schema(
-                        type=openapi.TYPE_OBJECT,
-                        properties={
-                            'src': openapi.Schema(type=openapi.TYPE_STRING, description='증강체 이미지 URL', example="https://example.com/image.png")
-                        }
-                    ),
-                }
-            )
-        ),
         'like_count': openapi.Schema(type=openapi.TYPE_INTEGER, description='좋아요 수', example=0),
         'dislike_count': openapi.Schema(type=openapi.TYPE_INTEGER, description='싫어요 수', example=0),
         'reroll_lv': openapi.Schema(type=openapi.TYPE_INTEGER, description='리롤 레벨', example=8),
