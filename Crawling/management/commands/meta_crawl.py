8e8bd23a2b93da5c1cbea0754638ad0780ad18bc
gns0314 <gns0314@naver.com>
2025-01-02 21:47:28 +0900

    ✨ : db, 크롤링 사이트들 데이터 유사도 검사 후 db저장 구현

--- Crawling/management/commands/meta_crawl.py
+++ Crawling/management/commands/meta_crawl.py
@@ -1,9 +1,8 @@
 from django.core.management.base import BaseCommand
 from Crawling.crawl.opgg_crawling import opgg_crawling
 from Crawling.crawl.lolchess_crawling import lolchess_crawling
-from Crawling.utils import reroll_lv
+from Crawling.utils import reroll_lv, jacaard_similarity
 from Meta.models import * 
-
 
 class Command(BaseCommand):
 
@@ -11,30 +10,65 @@ class Command(BaseCommand):
         lolchess = lolchess_crawling()
         opgg = opgg_crawling()
 
-        meta_data = {}
+        lolchess_duplicate_keys = set()
+        opgg_duplicate_keys = set()
 
-        # for data in meta_data:
-        #     meta, craeted = LolMeta.objects.get_or_create(title = data)
+        for lc_key, lc_value in lolchess.items():
+            for op_key, op_value in opgg.items():
+                if jacaard_similarity(lc_value['챔프'], op_value['챔프']) == 1:
+                    lolchess_duplicate_keys.add(lc_key)
+                    opgg_duplicate_keys.add(op_key)
+                    break 
 
-        #     champ_star = {1:0, 2:0, 3:0, 4:0, 5:0} 
-        #     for champ_name in meta_data[data]['챔프']:
-        #         champion, created = LolMetaChampion.objects.get_or_create(meta = meta, 
-        #                                                     champion = Champion.objects.get(name = champ_name),
-        #                                                     star = meta_data[data]['별'][champ_name], 
-        #                                                     location = meta_data[data]['위치'][champ_name])
+        for lol_key in lolchess_duplicate_keys:
+            del lolchess[lol_key]
+
+        for op_key in opgg_duplicate_keys:
+            del opgg[op_key]
+        
+        merge_meta_data = {**lolchess , **opgg}
+        merge_duplicate_keys = set()
+
+        db_meta_data = LolMeta.objects.all()
+        db_meta_champion = []
+
+        if db_meta_data:
+            for db_meta in db_meta_data:
+                db_meta_champion.append([meta_champ.champion.name for meta_champ in LolMetaChampion.objects.select_related('champion').filter(meta=db_meta)])
+
+        for merge_key, merge_value in merge_meta_data.items():
+            for db_meta in db_meta_champion:
+                if jacaard_similarity(merge_value['챔프'], db_meta) == 1:
+                    merge_duplicate_keys.add(merge_key)
+                    break 
+        
+        for merge_key in merge_duplicate_keys:
+            del merge_meta_data[merge_key]
+
+        meta_data = merge_meta_data
+
+        for data in meta_data:
+            meta, craeted = LolMeta.objects.get_or_create(title = data)
+
+            champ_star = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0} 
+            for champ_name in meta_data[data]['챔프']:
+                champion, created = LolMetaChampion.objects.get_or_create(meta = meta, 
+                                                            champion = Champion.objects.get(name = champ_name.replace(' ', '')),
+                                                            star = meta_data[data]['별'][champ_name], 
+                                                            location = meta_data[data]['위치'][champ_name])
                 
-        #         price = Champion.objects.get(name = champ_name).price
+                price = Champion.objects.get(name = champ_name.replace(' ', '')).price
 
-        #         champ_star[price] += meta_data[data]['별'][champ_name]
+                champ_star[price] += meta_data[data]['별'][champ_name]
 
-        #         champ_item = meta_data[data]['아이템'].get(champ_name, [])
-        #         if len(champ_item) > 0 :
-        #             for item in champ_item:
-        #                 if Item.objects.filter(name=item).exists():
-        #                     champion.item.add(Item.objects.filter(name=item).first())
+                champ_item = meta_data[data]['아이템'].get(champ_name, [])
+                if len(champ_item) > 0 :
+                    for item in champ_item:
+                        if Item.objects.filter(name=item).exists():
+                            champion.item.add(Item.objects.filter(name=item).first())
 
-        #     max_value = max(champ_star.values())
-        #     max_keys = [key for key, value in champ_star.items() if value == max_value]
+            max_value = max(champ_star.values())
+            max_keys = [key for key, value in champ_star.items() if value == max_value]
 
-        #     meta.reroll_lv = reroll_lv(max(max_keys))
-        #     meta.save()
+            meta.reroll_lv = reroll_lv(max(max_keys))
+            meta.save()
