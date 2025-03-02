3a2e6943be3a5db1003bb97156b6fc26e817400a
gns0314 <gns0314@naver.com>
2025-03-03 01:00:39 +0900

    🔥 : 미사용 라이브러리 제거

--- Crawling/crawl/tactics_crawling.py
+++ Crawling/crawl/tactics_crawling.py
@@ -1,8 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 
 def tactics_crawling():
