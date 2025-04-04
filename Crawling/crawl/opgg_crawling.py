4c3c9ec8d2b3ed3fd9e34328b7015446cf6c4e5d
gns0314 <gns0314@naver.com>
2025-04-05 01:52:15 +0900

    🐛 : 배포환경 버튼 셀렉터 변경

--- Crawling/crawl/opgg_crawling.py
+++ Crawling/crawl/opgg_crawling.py
@@ -34,7 +34,7 @@ def opgg_crawling():
         meta_title.append(meta.find_element(By.CSS_SELECTOR, 'div.flex.items-center.gap-1.text-\[12px\].leading-\[16px\].text-gray-0.md\:w-full.md\:gap-\[8px\].md\:text-\[14px\].md\:leading-\[20px\] > strong').text)
         meta_champ.append([champ.get_attribute('alt') for champ in meta.find_elements(By.CSS_SELECTOR, 'div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > img')])
 
-        button = meta.find_element(By.CSS_SELECTOR, 'div.flex.w-full.flex-row.items-center.justify-between.gap-2.md\\:basis-\\[400px\\].md\\:flex-col.md\\:items-start.md\\:px-0.md\\:py-\\[15px\\].md\\:pr-\\[16px\\] > button')
+        button = meta.find_element(By.CSS_SELECTOR, 'div.flex.w-full.flex-row.items-center.justify-between.gap-2.md\:basis-\[400px\].md\:flex-col.md\:items-start.md\:px-0.md\:py-\[15px\].md\:pr-\[16px\] > button')
         driver.execute_script("arguments[0].scrollIntoView(true);", button)
         time.sleep(0.3)
         button.click()
